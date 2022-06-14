d5ba0b9832c82573ea9c5235c518cc7fa575d9f4
Sarah Dylan <SarahmDylan@gmail.com>
2022-06-13 19:39:51 -0700

    Finish Project
    Completed the project, added slider to change grid size and a button
    clear the grid, and allowed the color of the squares to be randomized.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-const containerDiv = document.querySelector('.container')
+const containerDiv = document.querySelector('.container');
+const slider = document.querySelector('.slider');
+const gridText = document.querySelector('.gridText');
+const replacementDiv = document.createElement('div');
+const clearGridbutton = document.querySelector('#clearGrid')
+
+replacementDiv.classList.add('container');
+
+var gridPercentage = 5.787;
 
 for (i = 0; i < 256; i++) {
     let div = document.createElement('div');
@@ -7,9 +15,37 @@ for (i = 0; i < 256; i++) {
     containerDiv.append(div);
 }
 
+var grid = document.querySelectorAll('.grid');
+grid.forEach(square => square.addEventListener('mouseover', fillGrid));
+
 function fillGrid(e) {
-    this.classList.toggle('clickedGrid')
+    this.classList.add('clickedGrid')
+    this.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)},
+                                      ${Math.floor(Math.random() * 255)},
+                                      ${Math.floor(Math.random() * 255)})`
 }
 
-const grid = document.querySelectorAll('.grid')
-grid.forEach(square => square.addEventListener('click', fillGrid))
+function buildGrid(num) {
+    grid.forEach(square => containerDiv.removeChild(square))
+    gridPercentage = ((864-(num * 4)) / 8.64) / num;
+    for (i = 0; i < num * num; i++) {
+        let div = document.createElement('div');
+        div.classList.toggle('grid');
+        div.classList.add(`${i}`);
+        div.style.flexBasis = `${gridPercentage}%`;
+        console.log(div.style.flexBasis)
+        containerDiv.append(div);
+    }
+    grid = document.querySelectorAll('.grid');
+    grid.forEach(square => square.addEventListener('mouseover', fillGrid));
+}
+
+slider.oninput = function() {
+    gridText.textContent = `Grid Size: ${this.value} x ${this.value}`
+    buildGrid(this.value)
+}
+
+clearGridbutton.addEventListener('click', function() {
+    let clickedGrid = document.querySelectorAll('.clickedGrid');
+    clickedGrid.forEach(square => square.style.backgroundColor = '');
+})
